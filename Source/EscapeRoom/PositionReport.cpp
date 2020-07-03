2a68f427ecb47d9a1d6e8a048fd58276335c7fc9
ming lee <ming.lee@hotmail.com>
2020-07-03 14:21:50 +0100

    log items name.

--- Source/EscapeRoom/PositionReport.cpp
+++ Source/EscapeRoom/PositionReport.cpp
@@ -1,5 +1,6 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
+#include "GameFramework/Actor.h"
 #include "PositionReport.h"
 
 
@@ -18,9 +19,8 @@ UPositionReport::UPositionReport()
 void UPositionReport::BeginPlay()
 {
 	Super::BeginPlay();
-
-	// ...
-	
+	FString ObjectName = GetOwner()->GetName();
+	UE_LOG(LogTemp, Warning, TEXT("Position report for %s"), *ObjectName);
 }
 
 
